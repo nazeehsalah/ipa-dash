af9641e214f8db9699b0f3a71d74f6981bf203cd
nazeeh salah <nazeehsalah28@gmail.com>
2019-03-04 10:07:27 +0200

    attendence edit

--- js/attend.js
+++ js/attend.js
@@ -23,105 +23,93 @@ $(function () {
       datesList = getDates(new Date(couresList[$(this).val()].data.startDate), new Date()),
       tabelContent = "",
       index = 1
-    datesList.forEach(function (d) {
+    /* datesList.forEach(function (d) {
       tabelHeader += '<th class="center">' + d + '</th>'
-    })
-    $("#datatable-buttons thead tr").append(tabelHeader)
-    firebase.database().ref("attendence")
-      .orderByChild("courseKey").equalTo(couresList[$(this).val()].key).once("value")
+    }) */
+    /* $("#datatable-buttons thead tr").append(tabelHeader) */
+    firebase.database().ref("courseInfo/" + couresList[$(this).val()].key)
+      .once("value")
       .then(function (s) {
-        if (s.val() != null) {
-          var usersKeysIndex = 0;
-          Object.keys(s.val()).forEach(function (k) {
-            s.val()[k].usersKeys.forEach(function (u) {
-              firebase.database().ref("users")
-                .orderByKey().equalTo(u.userKey).once("value").then(function (us) {
-                  usersKeysIndex++;
-                  Object.keys(us.val()).forEach(function (k) {
-                    tabelContent = '<tr><td>' + index + '</td>' +
-                      '<td>' + us.val()[k].name + '</td>'
-                  })
-                  var dayFound = false;
-                  for (var i = 0; i < datesList.length; i++) {
-                    dayFound = false
-                    for (var j = 0; j < u.days.length; j++) {
-                      if (datesList[i] == u.days[j].date) {
-
-                        dayFound = true;
-                        tabelContent += '<td><img src="' + u.days[j].usersData.atteendSign +
-                          '" style="width: 75px;"><p>' + u.days[j].usersData.attendTime + ' </p></td>'
-                        break
+        Object.keys(s.val().attendence).forEach(function (dayKey) {
+          tabelHeader += '<th class="center">' + dayKey + '</th>'
+        })
+        Object.keys(s.val().users).forEach(function (userKey) {
+          console.log(userKey)
+          firebase.database().ref("users/" + userKey)
+            .once("value")
+            .then(function (userdata) {
+              tabelContent += '<tr>' +
+                '<td>' + index + '</td>' +
+                '<td>' + userdata.val().name + '</td>'
+              Object.keys(s.val().attendence).forEach(function (dayKey) {
+                tabelContent += '<td>' + (s.val().attendence[dayKey][userKey] != undefined ? '<img src="' + s.val().attendence[dayKey][userKey].atteendSign +
+                  '" style="width: 75px;display:block"><p>' + s.val().attendence[dayKey][userKey].attendTime + ' </p>' : "لم يحضر") + '</td>'
+              })
+              tabelContent += '</tr>'
+              index++;
+            }).then(function () {
+              if (Object.keys(s.val().users).indexOf(userKey) == Object.keys(s.val().users).length - 1) {
+                $("#datatable-buttons thead tr").append(tabelHeader)
+                $("#datatable-buttons tbody").append(tabelContent)
+                $("#datatable-buttons").length && $("#datatable-buttons").DataTable({
+                  dom: "Blfrtip",
+                  buttons: [
+                    {
+                      extend: 'print',
+                      exportOptions: {
+                        stripHtml: false,
+                        format: {
+                          body: function (inner, coldex, rowdex) {
+                            if (inner.length &= 0) return inner;
+                            var el = $.parseHTML(inner);
+                            var result = '';
+                            $.each(el, function (index, item) {
+                              if (item.nodeName == '#text') result = result + item.textContent;
+                              else if (item.nodeName == 'SUP') result = result + item.outerHTML;
+                              else if (item.nodeName == 'STRONG') result = result + item.outerHTML;
+                              else if (item.nodeName == 'IMG') result = result + item.outerHTML;
+                              else result = result + item.innerText;
+                            });
+                            return result;
+                          }
+                        }
                       }
-                      if (!dayFound)
-                        tabelContent += '<td>لم يحضر هذا اليوم</td>'
-                      if (i == datesList.length - 1)
-                        tabelContent += "</tr>"
-                    }
-                  }
-                  index++;
-                  $("#datatable-buttons tbody").append(tabelContent)
-                  if (usersKeysIndex == s.val()[k].usersKeys.length) {
-                    
-                    $("#datatable-buttons").length && $("#datatable-buttons").DataTable({
-                      dom: "Blfrtip",
-                      buttons: [
-                        {
-                          extend: 'print',
-                          exportOptions: {
-                            stripHtml: false,
-                            format: {
-                              body: function (inner, coldex, rowdex) {
-                                if (inner.length &= 0) return inner;
-                                var el = $.parseHTML(inner);
-                                var result = '';
-                                $.each(el, function (index, item) {
-                                  if (item.nodeName == '#text') result = result + item.textContent;
-                                  else if (item.nodeName == 'SUP') result = result + item.outerHTML;
-                                  else if (item.nodeName == 'STRONG') result = result + item.outerHTML;
-                                  else if (item.nodeName == 'IMG') result = result + item.outerHTML;
-                                  else result = result + item.innerText;
-                                });
-                                return result;
-                              }
-                            }
+                    },
+                    {
+                      extend: 'csv',
+                      exportOptions: {
+                        stripHtml: false,
+                        format: {
+                          body: function (inner, coldex, rowdex) {
+                            if (inner.length &= 0) return inner;
+                            var el = $.parseHTML(inner);
+                            var result = '';
+                            $.each(el, function (index, item) {
+                              if (item.nodeName == '#text') result = result + item.textContent;
+                              else if (item.nodeName == 'SUP') result = result + item.outerHTML;
+                              else if (item.nodeName == 'STRONG') result = result + item.outerHTML;
+                              else if (item.nodeName == 'IMG') result = result + item.src;
+                              else result = result + item.innerText;
+                            });
+                            return result;
                           }
-                        },
-                        {
-                          extend: 'csv',
-                          exportOptions: {
-                            stripHtml: false,
-                            format: {
-                              body: function (inner, coldex, rowdex) {
-                                if (inner.length &= 0) return inner;
-                                var el = $.parseHTML(inner);
-                                var result = '';
-                                $.each(el, function (index, item) {
-                                  if (item.nodeName == '#text') result = result + item.textContent;
-                                  else if (item.nodeName == 'SUP') result = result + item.outerHTML;
-                                  else if (item.nodeName == 'STRONG') result = result + item.outerHTML;
-                                  else if (item.nodeName == 'IMG') result = result + item.src;
-                                  else result = result + item.innerText;
-                                });
-                                return result;
-                              }
-                            }
-                          }
-                        },
-                      ],
-                      responsive: !0
-                    })
-                    $("#datatable-buttons").css("width", "100%")
-                    $(".x_panel").css("display", "inline-block");
-
-                  }
+                        }
+                      }
+                    },
+                  ],
+                  responsive: !0
                 })
+                $("#datatable-buttons").css("width", "100%")
+                $(".x_panel").css("display", "inline-block");
+              }
             })
-          })
-        } else {
-          console.log("no attendence")
-        }
+
+        })
+
       })
-  });
+  })
+
+
 })
 
 function getDates(startDate, endDate) {
