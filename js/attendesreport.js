75431a90b75b2b3c1a5ad08ac73466eb5000878d
nazeeh salah <nazeehsalah28@gmail.com>
2019-08-11 22:31:20 +0200

    new Report For attendence 11/8/2019

---
+++ js/attendesreport.js
@@ -0,0 +1,50 @@
+$(function () {
+  content()
+  setInterval(content,900000);
+})
+function content (){
+  console.log("ffffffffffffffffffffffffffffffffffff")
+  $("#content").empty();
+  var found = true,
+  today = new Date() //new Date(),
+coursesData = []
+// this code get run courses
+firebase.database().ref("/courses")
+  .orderByChild("run")
+  .equalTo(true)
+  .once("value")
+  .then(function (snapshot) {
+    Object.keys(snapshot.val()).forEach(function (key, i) {
+      if (new Date(snapshot.val()[key].startDate) < new Date(today) && new Date(snapshot.val()[key].endDate) > new Date(today)) {
+        found = false;
+        firebase.database().ref("/courseInfo/" + key)
+          .once("value")
+          .then(function (snap) {
+            var courseObj = {}
+            courseObj.name = snapshot.val()[key]["name"]
+            courseObj.total = Object.keys(snap.val().users).length;
+            if (snap.val().attendence != undefined)
+              courseObj.attende = snap.val().attendence[today.toDateString()] == undefined ? 0 : Object.keys(snap.val().attendence[today.toDateString()]).length;
+            else
+              courseObj.attende = 0
+            courseObj.absance = courseObj.total - courseObj.attende
+            coursesData.push(courseObj);
+            var content = ''
+            content = '<div class="col-md-6"><div class="x_panel"><div class="x_title text-center"><h2 class="text-center" style="float: none">' + courseObj.name + '</h2><div class="clearfix"></div></div><div class="x_content"><div class="col-md-12 col-sm-12 col-xs-12"><ul class="stats-overview"><li><span class="name"> عدد الحضور</span><span class="value text-success"> ' + courseObj.attende + ' </span></li><li class="hidden-phone"><span class="name"> عدد الغياب</span><span class="value text-success"> ' + courseObj.absance + ' </span></li><li><span class="name"> عدد المشتركين </span><span class="value text-success"> ' + courseObj.total + ' </span></li></ul><br /></div></div></div></div>'
+            $("#content").append(content);
+            if (i == Object.keys(snapshot.val()).length - 1) {
+              if (coursesData.length == 0) {
+                $("#content").append("<h1 class=text-center style='margin-top:100px'>لا توجد برامج تعمل اليوم جرب غدا</h1>")
+              }
+            }
+          })
+      } else {
+        if (found && i == Object.keys(snapshot.val()).length - 1) {
+          $("#content").append("<h1 class=text-center style='margin-top:100px'>لا توجد برامج تعمل اليوم جرب غدا</h1>")
+        }
+
+      }
+    })
+
+  })
+}
